ae8c26c8af28455fb5b28567718b6d3e3495bef6
KarinaT <tarasenkoka@gmail.com>
2013-11-15 13:48:30 +0200

    Update WebDriverFactory.java

--- src/main/java/com/epam/preproduction/helpers/core/WebDriverFactory.java
+++ src/main/java/com/epam/preproduction/helpers/core/WebDriverFactory.java
@@ -107,7 +107,7 @@ public class WebDriverFactory {
 		}
 		if (browserType.equals("opera")) {
 			System.setProperty("opera.binary", PropertyReader.getOperaBinaryPath());
-			return new OperaDriver();
+			return new OperaDriver(capabilities);
 		}
 		throw new Error("Unrecognized browser type: " + browserType);
 	}
