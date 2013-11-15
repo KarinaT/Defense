b6a938239a6acf52d5b7ce2c82f186a705da23a3
KarinaT <tarasenkoka@gmail.com>
2013-11-15 15:17:08 +0200

    Update ScreenShotOnFailure.java

--- src/main/java/com/epam/preproduction/configuration/ScreenShotOnFailure.java
+++ src/main/java/com/epam/preproduction/configuration/ScreenShotOnFailure.java
@@ -19,30 +19,21 @@ import com.epam.preproduction.helpers.core.WebDriverFactory;
 
 public class ScreenShotOnFailure extends TestListenerAdapter {
 
-	  private static final String NAME = "<a href=\"%s\"><img src=\"%<s\" width=200 height=150></a><br>";
-	  private static WebDriver driver;
+	@Override
+	public void onTestFailure(ITestResult tr) {
+		WebDriver driver = WebDriverFactory.getDriver(DesiredCapabilities.firefox());
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		DateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyy__hh_mm_ssaa");
+		String destDir = "target/surefire-reports/html/screenshots";
+		new File(destDir).mkdirs();
+		String destFile = dateFormat.format(new Date()) + ".png";
 
-	   @Override
-	   public void onTestFailure(ITestResult tr) {
-	    File scrFile = ((TakesScreenshot) driver)
-	      .getScreenshotAs(OutputType.FILE);
-	    DateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyy__hh_mm_ssaa");
-	    String destDir = "target/surefire-reports/html/screenshots/";
-	    new File(destDir).mkdirs();
-	    String destFile = dateFormat.format(new Date()) + ".png";
-	    System.out.println(destFile);
-	    System.setProperty("org.uncommons.reportng.escape-output", "false");
-	    System.out.println(destDir);
-	    try {
-	     FileUtils.copyFile(scrFile, new File(destDir + "/" + destFile));
-	    } catch (IOException e) {
-	     e.printStackTrace();
-	    }
-	    Reporter.log(String.format(NAME, "screenshots/" + destFile));
+		try {
+			FileUtils.copyFile(scrFile, new File(destDir + "/" + destFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-	   }
-	   
-	   public static void setDriver(WebDriver d){
-	    driver = d;
-	   }
+		Reporter.log("Saved <a href=screenshots" + destFile	+ ">Screenshot</a>");
+	}
 }
