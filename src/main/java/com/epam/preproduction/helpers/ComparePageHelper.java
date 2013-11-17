e3a38c6124492aadc1ac74c803f404330db2deb2
KarinaT <tarasenkoka@gmail.com>
2013-11-18 00:39:39 +0200

    Update ComparePageHelper.java

--- src/main/java/com/epam/preproduction/helpers/ComparePageHelper.java
+++ src/main/java/com/epam/preproduction/helpers/ComparePageHelper.java
@@ -18,7 +18,7 @@ public class ComparePageHelper {
 	}
 
 	public List<Item> grabAllParams() {
-		Reporter.log("checking if items have similar characteristics");
+		Reporter.log("Checking if items have similar characteristics" + "br");
 		List<Item> microwaves = new ArrayList<Item>();
 		Item item1 = new Microwave();
 		Item item2 = new Microwave();
